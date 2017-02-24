78cf6dc8714da97998e257a902623eec82bf3da2
Timmel <tim@q42.nl>
2017-02-24 15:58:36 +0100

    Added comments

--- src/Q42.ImageResizert/UrlHelperExtension.cs
+++ src/Q42.ImageResizert/UrlHelperExtension.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http.Extensions;
-using Microsoft.AspNetCore.Mvc.Routing;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
 using System;
 
 namespace Q42.ImageResizert
@@ -14,21 +12,47 @@ namespace Q42.ImageResizert
       _storageSettings = storageSettings.Value;
     }
 
+    /// <summary>
+    /// Resize image and cover
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    /// <returns></returns>
     public string GetUrlForCoverImage(Guid id, int width, int height)
     {
       return GetUrlForImage(id.ToString().ToUpper(), width, height, true);
     }
 
+    /// <summary>
+    /// Resize image and cover
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    /// <returns></returns>
     public string GetUrlForCoverImage(string id, int width, int height)
     {
       return GetUrlForImage(id, width, height, true);
     }
 
+    /// <summary>
+    /// Resize image while maintaining aspect ratio
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    /// <returns></returns>
     public string GetUrlForScaledImage(Guid id, int width = 0, int height = 0)
     {
       return GetUrlForImage(id.ToString().ToUpper(), width, height, false);
     }
 
+    /// <summary>
+    /// Distinguish between CDN or image resize url
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public string GetBaseUrlForImage(Guid id)
     {
       return GetBaseUrlForImage(id.ToString().ToUpper());
