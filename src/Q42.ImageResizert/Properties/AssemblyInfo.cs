1f01a48dd1d28e8ef38520b45f5cd6c3382e2394
Michiel Post <michiel@q42.nl>
2017-05-05 15:44:27 +0200

    Feature/package metadata (#3)
    * Added some NuGet package metadata
    
    * more

--- src/Q42.ImageResizert/Properties/AssemblyInfo.cs
+++ src/Q42.ImageResizert/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Q42")]
 [assembly: AssemblyProduct("Q42.ImageResizer")]
 [assembly: AssemblyTrademark("")]
 
